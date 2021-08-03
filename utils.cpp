28d74acfdf163c830d4e4195e1014a3c975cc88f
james-row <james@nd.com.cn>
2021-08-03 09:38:39 +0800

    add utils.cpp

---
+++ utils.cpp
@@ -0,0 +1,99 @@
+#include "utils.h"
+
+
+int sig_pipefd[2];
+
+void sig_handler( int sig)
+{
+    int save_errno = errno;
+    int msg = sig;
+    send(sig_pipefd[1], (char *)&msg, 1, 0);
+}
+
+void addsig(int sig, void(handler)(int), bool restart)
+{
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = handler;
+    if (restart)
+    {
+        sa.sa_flags |= SA_RESTART;
+    }
+    sigfillset(&sa.sa_mask);
+    assert(sigaction(sig, &sa, NULL) != -1);
+}
+
+int setnonblocking( int fd)
+{
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+
+void addfd(int epollfd, int fd)
+{
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN | EPOLLET;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnonblocking(fd);
+}
+
+void removefd(int epollfd, int fd)
+{
+    if (epollfd >= 0)
+    {
+        epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, 0);
+    }
+    close(fd);
+}
+
+static const int CTRL_LEN = CMSG_LEN(sizeof(int));
+
+void send_fd(int fd, int fd_to_send)
+{
+    struct iovec iov[1];
+    struct msghdr msg;
+    char buf[1];
+
+    iov[0].iov_base = buf;
+    iov[0].iov_len = 1;
+    msg.msg_name = NULL;
+    msg.msg_namelen = 0;
+    msg.msg_iov = iov;
+    msg.msg_iovlen = 1;
+
+    cmsghdr cm;
+    cm.cmsg_len = CTRL_LEN;
+    cm.cmsg_level = SOL_SOCKET;
+    cm.cmsg_type = SCM_RIGHTS;
+    *(int *)CMSG_DATA( &cm ) = fd_to_send;
+    msg.msg_control = &cm;
+    msg.msg_controllen = CTRL_LEN;
+
+    sendmsg(fd, &msg, 0);
+}
+
+int recv_fd(int fd)
+{
+    struct iovec iov[1];
+    struct msghdr msg;
+    char buf[0];
+
+    iov[0].iov_base = buf;
+    iov[0].iov_len = 1;
+    msg.msg_name = NULL;
+    msg.msg_namelen = 0;
+    msg.msg_iov = iov;
+    msg.msg_iovlen = 1;
+
+    cmsghdr cm;
+    msg.msg_control = &cm;
+    msg.msg_controllen = CTRL_LEN;
+
+    recvmsg(fd, &msg, 0);
+    int fd_to_read = *(int *)CMSG_DATA(&cm);
+
+    return fd_to_read;
+}
